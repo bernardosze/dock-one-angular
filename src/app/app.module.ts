7e25dd2c1784ff25a7e39b5c13c4ec6e1763ab34
Bernardo Sze <bernardo.sze@gmail.com>
2019-09-16 10:44:15 -0400

    initial posting settup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { 
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatToolbarModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatToolbarModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
